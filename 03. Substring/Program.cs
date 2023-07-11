ef098cfd52a379e60dee82aa674e31c6ac74b2dd
Emberclap <133802297+Emberclap@users.noreply.github.com>
2023-07-11 13:53:46 +0300

    Add project files.

---
+++ 03. Substring/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyWord = Console.ReadLine();
+            string words = Console.ReadLine();
+            
+            while (words.Contains(keyWord))
+            {
+                int index = words.IndexOf(keyWord);
+                words = words.Remove(index, keyWord.Length);
+            }
+            Console.WriteLine(words);
+
+        }
+    }
+}
---
+++ 05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string digits = "";
+            string letters = "";
+            string symbols = "";
+
+            foreach (var character in text)
+            {
+                if (Char.IsDigit(character))
+                {
+                    digits += character;
+                }
+                else if (Char.IsLetter(character))
+                {
+                    letters += character;
+                }
+                else
+                {
+                    symbols += character;
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(symbols);
+        }
+    }
+}
